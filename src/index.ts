b889b192de04a1b02dcac62e7ff74b0b25881c9c
ss49919201 <sakashin10291029@gmail.com>
2024-09-10 09:07:05 +0900

    texts api

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,14 @@
 import { Hono } from "hono";
+import { TextRepository } from "./infrascructure/memory/textRepository";
 
 const app = new Hono<{ Bindings: CloudflareBindings }>();
 
 app.get("/", (c) => {
   return c.text("Hello Hono🔥");
+});
+
+app.get("/texts", async (c) => {
+  return c.json(await new TextRepository().getAll());
 });
 
 export default app;
